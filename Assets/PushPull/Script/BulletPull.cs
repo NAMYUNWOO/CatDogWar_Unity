da1919c942419a5d8a850622a9b6616ea64b0128
NAMYUNWOO <nyw8902@gmail.com>
2018-05-13 12:42:09 +0900

    first commit

---
+++ Assets/PushPull/Script/BulletPull.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletPull : Bullet {
+
+	void OnTriggerEnter2D(Collider2D collider){
+		if (collider.gameObject.tag == "TargetCoin" || collider.gameObject.tag == "TargetSkull") {
+			//Vector2 targetpos = collider.gameObject.transform.position;
+
+			collider.gameObject.GetComponent<Rigidbody2D> ().AddForce(new Vector2 (base.pullPower, 0f));
+	
+
+		}
+		//Destroy (gameObject);
+		base.OnTriggerEnter2D (collider);
+
+	}
+}
---
+++ Assets/PushPull/Script/BulletPush.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class BulletPush : Bullet {
+
+	void OnTriggerEnter2D(Collider2D collider){
+		if (collider.gameObject.tag == "TargetCoin" || collider.gameObject.tag == "TargetSkull") {
+			//Vector2 targetpos = collider.gameObject.transform.position;
+			collider.gameObject.GetComponent<Rigidbody2D> ().AddForce(new Vector2 (base.pushPower, 0f));
+
+		}
+		base.OnTriggerEnter2D (collider);
+	}
+}
